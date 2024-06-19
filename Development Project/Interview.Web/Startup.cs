5db9440f5c95a577f9c9d5989503ceb242216f50
Smriti04 <39514360+Smriti04@users.noreply.github.com>
2024-06-19 01:04:11 -0400

    Removed redundant code

--- Development Project/Interview.Web/Startup.cs
+++ Development Project/Interview.Web/Startup.cs
@@ -1,4 +1,3 @@
-using Interview.Web.Extension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -53,7 +52,6 @@ namespace Interview.Web
             {
                 endpoints.MapControllers();
             });
-            app.InitializeDb();
         }
     }
 }
